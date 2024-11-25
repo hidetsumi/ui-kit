6675781744e5e50ae694d558cff9bf39f19088bc
hidetsumi <hidetsu99@gmail.com>
2024-11-24 22:44:14 -0300

    Theme mode, dynamic imports, creation toggle button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/toggle-button-theme";
 
 export default function Home() {
   return (
     <div className="grid">
-
+      <ModeToggle />
+      <Button />
     </div>
   );
 }
--- src/stories/input/Button/Button.stories.ts
+++ src/stories/input/Button/Button.stories.ts
@@ -2,7 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
 import { Button } from './Button';
-import { STORY_BOOK } from '@/config';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
