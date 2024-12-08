9504b16e9519161dd03ff4658bdb0bf73ff4058a
hidetsumi <hidetsu99@gmail.com>
2024-12-07 21:31:19 -0300

    fix: fix cssglobals for storybook

--- src/stories/input/Button/Button.stories.ts
+++ src/stories/input/Button/Button.stories.ts
@@ -1,12 +1,12 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import { Button } from './Button';
+import { ButtonStorybook } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Inputs/Button',
-  component: Button,
+  component: ButtonStorybook,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
@@ -14,12 +14,12 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+  // argTypes: {
+  //   backgroundColor: { control: 'color' },
+  // },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof ButtonStorybook>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -27,27 +27,27 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    children: 'Button',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
+    variant: 'secondary',
+    children: 'Secondary'
   },
 };
 
 export const Large: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
+    size: 'lg',
+    children: 'Button',
   },
 };
 
 export const Small: Story = {
   args: {
-    size: 'small',
-    label: 'Button',
+    size: 'sm',
+    children: 'Button',
   },
 };
--- src/stories/input/Button/Button.tsx
+++ src/stories/input/Button/Button.tsx
@@ -1,36 +1,32 @@
+import { Button, ButtonProps } from '@/components/ui/button';
 import React from 'react';
 
-import './button.css';
 
-export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large';
-  /** Button contents */
-  label: string;
-  /** Optional click handler */
-  onClick?: () => void;
-}
+// export interface ButtonProps {
+//   /** Is this the principal call to action on the page? */
+//   primary?: boolean;
+//   /** What background color to use */
+//   backgroundColor?: string;
+//   /** How large should the button be? */
+//   // size?: 'small' | 'medium' | 'large';
+//   /** Button contents */
+//   label: string;
+//   /** Optional click handler */
+//   onClick?: () => void;
+// }
 
 /** Primary UI component for user interaction */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
+export const ButtonStorybook = ({
+  children,
   ...props
 }: ButtonProps) => {
-  // const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button
+    <Button
       type="button"
       className="flex"
       {...props}
     >
-      {label}
-    </button>
+      {children}
+    </Button>
   );
 };
